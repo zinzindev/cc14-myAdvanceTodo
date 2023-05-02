52dafb54c0571c93fd0799116dcce0e5e2259166
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-02 14:23:29 +0700

    refactor-custom-hook

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,18 +1,22 @@
 import { useContext } from 'react';
 import { TodoContext } from '../contexts/TodoContext';
 
-export const useTodo = () => {
-    // Consumer
-    const sharedObj = useContext(TodoContext);
-    return sharedObj;
-    /*sharedObj={
-		todos,
-		todosFilter,
-		addTodo,
-		editTodo,
-		deleteTodo,
-		selectList,
-		searchTodo,
-	}
-	*/
-};
+// export const useTodo = () => {
+//     // Consumer
+//     // const sharedObj = useContext(TodoContext);
+//     // return sharedObj;
+//     /*sharedObj={
+// 		todos,
+// 		todosFilter,
+// 		addTodo,
+// 		editTodo,
+// 		deleteTodo,
+// 		selectList,
+// 		searchTodo,
+// 	}
+// 	*/
+
+//     return useContext(TodoContext);
+// };
+
+export const useTodo = () => useContext(TodoContext);
