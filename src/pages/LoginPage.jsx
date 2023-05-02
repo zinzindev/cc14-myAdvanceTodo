4e6ff9ca02772abcb5fc0e03571d89f4b5394894
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-02 17:35:15 +0700

    prepare-multipage for routing

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+
+function LoginPage() {
+  return (
+	<div>LoginPage</div> 
+  )
+}
+
+export default LoginPage
+// rfc
+// rfce
+// rafc
+// rafce
+
+//<LoginPage/> => <div>LoginPage</div> 
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class ProfilePage extends Component {
+
+  render() {
+	return (
+	  <div>ProfilePage</div>
+	)
+  }
+}
+
+export default ProfilePage
+
+// <ProfilePage/> ==> เรียก method render ==> <div>ProfilePage</div>
