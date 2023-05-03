aeb8fab7e11c2417a9805fa873a6d2ab359dde96
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-03 15:54:50 +0700

    MUI:finish:profilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { MenuAppBar as AppBar } from '../components/Common/AppBar';
 // import Box from '@mui/material/Box'
-import { Box,Grid,Typography } from '@mui/material';
-import {UserAvatar} from '../components/Common/UserAvatar';
+import { Box, Grid, Typography, Button } from '@mui/material';
+import { UserAvatar } from '../components/Common/UserAvatar';
+import { Input } from '../components/Common/Input';
 
 class ProfilePage extends Component {
     render() {
@@ -10,25 +11,86 @@ class ProfilePage extends Component {
             <>
                 <AppBar />
                 <Box sx={{ flexGrow: 1 }}>
-					<Grid container spacing={8} sx={{padding:12}}>
-						<Grid item xs={10}>
-							<Typography variant="h1" component="h6">Edit Profile</Typography>
-						</Grid>
-						<Grid item xs={2}>
-							<UserAvatar 
-							sx={{width:120, height: 120}} 
-							alt="Smir Khan"
-							src="https://images.unsply1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-						</Grid>
-						<Grid item xs={6}>FirstName</Grid>
-						<Grid item xs={6}>LastName</Grid>
-						<Grid item xs={12}>Email</Grid>
-						<Grid item xs={12}>BirthDate</Grid>
-						<Grid item xs={6}>BTN-Update</Grid>
-						<Grid item xs={6}>BTN-Cancel</Grid>
-						<Grid item xs={12}>BTN-Logout</Grid>
-					</Grid>
-				</Box>
+                    <Grid container spacing={8} sx={{ padding: 20 }}>
+                        <Grid item xs={10} alignSelf="flex-end">
+                            <Typography variant='h1' component='h6' >
+                                Edit Profile
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <UserAvatar
+                                sx={{ width: 120, height: 120 }}
+                                alt='Amir Khan'
+                                src='https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Input
+                                label={<h1>First Name</h1>}
+                                placeholder='your first name'
+                                name='firstName'
+                                error={false}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Input
+                                label={<h1>Last Name</h1>}
+                                placeholder='your last name'
+                                name='lastName'
+                                error={false}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Input
+                                label={<h1>Last Name</h1>}
+                                placeholder='example@mail.com'
+                                name='email'
+                                type='email'
+                                error={false}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Input name='date' type='date' error={false} />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button
+                                fullWidth
+                                variant='contained'
+                                size='large'
+                                sx={{ background: '#db4c3f', padding: 2 }}
+                            >
+                                <Typography variant='h5' component='span'>
+                                    Edit Profile
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button
+                                fullWidth
+                                variant='contained'
+                                size='large'
+                                sx={{ backgroundColor: "#aaa" ,padding: 2 }}
+                            >
+                                <Typography variant='h5' component='span'>
+                                    Cancel
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                fullWidth
+                                variant='outlined'
+                                size='large'
+                                color='warning'
+                                sx={{ padding: 2 }}
+                            >
+                                <Typography variant='h5' component='span'>
+                                    Logout
+                                </Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
             </>
         );
     }
