290849e8bae33086e1a950b899610b5adb0eff95
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-03 16:51:46 +0700

    config tailwind and styled-component

--- src/components/Common/Spinner.jsx
+++ src/components/Common/Spinner.jsx
@@ -1,20 +1,26 @@
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+import styled from 'styled-components';
+
+// สร้าง Commponent
+const Wrapper = styled.div`
+    position: relative;
+    height: 100vh;
+
+    & > .loading__container {
+        display: flex;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+`;
 
 export function Spinner() {
     return (
-        <div className='relative h-screen'>
-            <Box
-                sx={{
-                    display: 'flex',
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: `translate(-${50}% , -${50}%)`,
-                }}
-            >
+        <Wrapper>
+            <div className='loading__container'>
                 <CircularProgress />
-            </Box>
-        </div>
+            </div>
+        </Wrapper>
     );
 }
